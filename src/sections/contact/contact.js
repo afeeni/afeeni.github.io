1e2192c825e22811766e9cac3f60a02f03495f63
afeeniphillips <afeeni.phillips@bison.howard.edu>
2022-03-03 08:01:12 -0800

    added simple contact form

--- src/sections/contact/contact.js
+++ src/sections/contact/contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import useStyles from "./styles";
 import styled from "styled-components";
 import { HashLink, NavHashLink } from 'react-router-hash-link';
@@ -14,12 +14,51 @@ const Wrapper = styled.section`
   background: #E0F8D1;
 `;
 
+  const [status, setStatus] = useState("Submit");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("Sending...");
+    const { name, email, message } = e.target.elements;
+    let details = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+    setStatus("Submit");
+    let result = await response.json();
+    alert(result.status);
+  };
+
+
 
     return(
 
         <Wrapper id="contact">
         <h1>Contact Me</h1>
 
+
+        <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" required />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" required />
+      </div>
+      <div>
+        <label htmlFor="message">Message:</label>
+        <textarea id="message" required />
+      </div>
+      <button type="submit">{status}</button>
+    </form>
 
         </Wrapper>
         
