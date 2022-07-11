1f55d249d85ea4e72c651302620aa7065732073d
afeeniphillips <afeeni.phillips@bison.howard.edu>
2022-07-11 16:30:05 -0700

    updated resume

--- src/sections/resume/resume.js
+++ src/sections/resume/resume.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import DownloadIcon from '@mui/icons-material/Download';
 import { Link } from "@mui/material";
 import { Button } from "@mui/material";
-import resume from '../../images/Afeeni Phillips - Full Resume.pdf'
+import resume from '../../images/AfeeniPhillipsResume.pdf'
 import { Document, Page, pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
